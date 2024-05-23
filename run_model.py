bde86d8343c8d495ebc41bbec7f50002a7f3bac8
egorehot <egor.d912@gmail.com>
2024-05-23 18:18:04 +0300

    run_experiments.py functional

--- run_model.py
+++ run_model.py
@@ -2,8 +2,14 @@ from abm_model.market_model import MarketModel
 
 
 def run():
-    model = MarketModel(1, 0, 30, 100)
+    model = MarketModel(
+        fundamentalists_number=0,
+        chartists_number=1,
+        steps_number=30,
+        initial_market_price=100.,
+    )
     model.run_model()
+    print(model.datacollector.get_model_vars_dataframe())
 
 
 if __name__ == '__main__':
